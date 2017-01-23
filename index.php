4f968f26eac808e99f342a0ab86eddf6678d38a2
Malik Zubayer Ul Haider <haider15-5902@diu.edu.bd>
2017-01-23 10:06:46 -0600

    Changed login

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo "Hello Rogue"; ?></title>
+</head>
+<body>
+	<div class="wrapper">
+		<form action="" id="LoginForm">
+			<h2>Login</h2>
+			<input type="text" placeholder="NAME">
+			<input type="password" placeholder="PASSWORD">
+			<input type="submit">
+		</form>
+	</div>
+
+	<link rel="stylesheet" href="style.css">
+</body>
+</html>
